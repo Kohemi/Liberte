fa95a287bd4f806e32e66acd72d4041c19a5eae1
JinsungGwag <jspartner1@naver.com>
2020-10-15 03:12:55 +0900

    Add Room Manager

---
+++ Bed_Time_Story/Assets/10PersonalFolder/Jinsung/Scripts/KidRoomMananger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KidRoomMananger : MonoBehaviour
+{
+    public GameObject dark;
+    public GameObject original;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadBeach()
+    {
+        SceneManager.LoadScene("Level2_Beach");
+    }
+
+    public void SetDark()
+    {
+        dark.SetActive(true);
+        original.SetActive(false);
+    }
+
+    public void SetOriginal()
+    {
+        original.SetActive(true);
+        dark.SetActive(false);
+    }
+}

254183be6b27a13e85c6e71f82af0b4cfab658b2
Hemi <58258336+Kohemi@users.noreply.github.com>
2020-10-16 10:58:12 +0900

    Add Cutscene transition function

---
+++ Bed_Time_Story/Assets/TimelinePlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelinePlayer : MonoBehaviour
+{
+    public PlayableDirector cut1; 
+    public PlayableDirector cut2;
+    public GameObject player;
+    public bool isCut1Ended;
+    public bool isStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            isCut1Ended = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isStarted = true;
+        }
+
+        if(isStarted == true)
+        {
+            cut1.Play();
+            Invoke("Cut1End", 5f);
+            
+
+        }
+
+        if(isCut1Ended == true)
+        {
+            cut2.Play();
+        }
+    }
+
+    public void StartTimeline()
+    {
+        cut1.Play();
+    }
+
+    void Cut1End()
+    {
+        isCut1Ended = true;
+    }
+
+}

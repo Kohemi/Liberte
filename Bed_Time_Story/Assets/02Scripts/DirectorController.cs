f0fb8fa6415488517ac5197ad454a5c1ec2bb9fb
kohemi <gostone11@gmail.com>
2020-10-19 20:44:04 +0900

    Upload Timeline

---
+++ Bed_Time_Story/Assets/02Scripts/DirectorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class DirectorController : MonoBehaviour
+{
+    public PlayableDirector cut1;
+    public PlayableDirector cut2;
+    public PlayableDirector cut3;
+    public PlayableDirector cut4;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        {
+            cut1.Play();
+        }
+        
+    }
+}
---
+++ Bed_Time_Story/Assets/02Scripts/VRInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VRInput : MonoBehaviour
+{
+    public Camera eventCamera = null;
+
+    public OVRInput.Button clickButton = OVRInput.Button.PrimaryIndexTrigger;
+    public OVRInput.Controller controller = OVRInput.Controller.All;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
